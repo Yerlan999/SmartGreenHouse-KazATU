89c067ad34fbf186f192b9df50b2706beeeb71f3
Amir Yerlan <fcbayernmunchenerlan999@gmail.com>
2022-01-18 01:15:21 +0600

    Parsing Issue

--- Automatic_GreenHouse/Arduino_to_ESP32_date_exchange/Arduino_to_ESP32_date_exchange.ino
+++ Automatic_GreenHouse/Arduino_to_ESP32_date_exchange/Arduino_to_ESP32_date_exchange.ino
@@ -1,20 +1,54 @@
-// CODE FOR ARDUINO MEGA
+// КОД ДЛЯ ARDUINO MEGA
 
 #define RXD2 0
 #define TXD2 1
 
 String from_ESP32;
+String to_ESP32;
+String a_ActuatorValue;  
+int start_s = 0;
+int end_s = 0;
+
+// Тестовые данные датчиков.
+float Sensors[5] = {1, -5, 10.99, -230.77, 17};
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   Serial1.begin(9600, SERIAL_8N1);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("Hello, ESP32! (P.S from Arduino)");
+
+// *************** БЛОК ОТПРАВКИ ЗНАЧЕНИИИ ДАТЧИКОВ НА ESP32 ***************
+
+  // Конвертация значении датчиков в СТРОКУ!!! для последующей отпавки.
+  for(int i=0; i<sizeof(Sensors)/sizeof(float); i++){
+    to_ESP32 += String(Sensors[i])+String(",");  // Разделение значении через ","
+  };
+ 
+  Serial.println(to_ESP32);   // Отправка данных на ESP32 через "Serial Port"
+  to_ESP32 = "";              // Стирание данных.
   delay(1500);
-  Serial.println(Serial.readString());
+  
+// *************************************************************************
+
+
+// *************** БЛОК ПРИЕМА ЗНАЧЕНИИИ АКТУАТОРОВ ОТ ESP32 ***************
+
+  // Прием СТРОКИ от ESP32(Значения актуаторов).
+    from_ESP32 = Serial.readString();
+    Serial.println(from_ESP32);       // Вывод всей СТРОКИ.
+  
+    // Парсинг(Разделение) значении через ",".
+//    for(int i=0; i<from_ESP32.length(); i++){
+//       if(String(from_ESP32.charAt(i))==String(",")){
+//         end_s = i;
+//         a_ActuatorValue = from_ESP32.substring(start_s, end_s);
+//         start_s = end_s+1;
+//         Serial.println(a_ActuatorValue);  // !!! Вывод значении каждого актуатора.
+//       };   
+//    };
+
+// *************************************************************************
   
 }
--- Automatic_GreenHouse/ESP32_from_Arduino_data_exchange/ESP32_from_Arduino_data_exchange.ino
+++ Automatic_GreenHouse/ESP32_from_Arduino_data_exchange/ESP32_from_Arduino_data_exchange.ino
@@ -1,21 +1,55 @@
-// CODE FOR ESP32
+// КОД ДЛЯ ESP32
 
 #define RXD2 16
 #define TXD2 17
 
 String from_Arduino;
+String to_Arduino;
+String a_SensorValue;  
+int start_s = 0;
+int end_s = 0;
+
+// Тестовые данные для актуаторов.
+float Actuators[5] = {1, -1, 1.99, -1.77, 1};
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   Serial1.begin(9600, SERIAL_8N1, RXD2, TXD2);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  from_Arduino = Serial1.readString();
-  Serial.println(from_Arduino);
 
-  Serial1.println("Hello, Arduino! (P.S from ESP32)");
+// *************** БЛОК ОТПРАВКИ ЗНАЧЕНИИИ АКТУАТОРОВ НА ARDUINO ***************
+
+  // Конвертация значении актуаторов в СТРОКУ!!! для последующей отпавки.
+  for(int i=0; i<sizeof(Actuators)/sizeof(float); i++){
+    to_Arduino += String(Actuators[i])+String(",");  // Разделение значении через ","
+  };
+
+  Serial1.println(to_Arduino);       // Отправка данных на Arduino через "Serial Port"
+  to_Arduino = "";
   delay(600);
+  
+// *************************************************************************
+
+
+// *************** БЛОК ПРИЕМА ЗНАЧЕНИИИ ДАТЧИКОВ ОТ ARDUINO ***************
+
+    // Прием СТРОКИ от Arduino(Значения датчиков).
+    from_Arduino = Serial1.readString();
+    Serial.println(from_Arduino);       // Вывод всей СТРОКИ.
+    
+    // Парсинг(Разделение) значении через ",".
+//    for(int i=0; i<from_Arduino.length(); i++){
+//       if(String(from_Arduino.charAt(i))==String(",")){
+//         end_s = i;
+//         a_SensorValue = from_Arduino.substring(start_s, end_s);
+//         start_s = end_s+1;
+//         Serial.println(a_SensorValue);  // !!! Вывод значении каждого датчика.
+//       };   
+//    };
+
+// *************************************************************************
+
+
 }
